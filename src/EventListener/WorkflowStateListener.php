9c55db68d959e3139dc313514bb2bf56ec198e67
imaximius <imaximius@gmail.com>
2021-05-27 15:24:04 +0300

    Auto detecting of a state field was added

--- src/EventListener/WorkflowStateListener.php
+++ src/EventListener/WorkflowStateListener.php
@@ -7,6 +7,7 @@ namespace Imaximius\WorkflowBundle\EventListener;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Workflow\Registry;
@@ -34,11 +35,34 @@ class WorkflowStateListener implements EventSubscriber
      */
     public function getSubscribedEvents(): array
     {
-        return [Events::preUpdate];
+        return [
+            Events::prePersist,
+            Events::preUpdate,
+        ];
     }
 
     /**
-     * Update User in the Index.
+     * Iniciate state
+     */
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $class = $args->getObject();
+        if (!in_array(get_class($class), $this->arguments['supportsWorkflow'], true)) {
+            return;
+        }
+
+        $stateField = $this->getWorkflowName($class);
+        $initialState = $this->getInitialState($class);
+        $currentState = $class->{'get'.ucfirst($stateField)}();
+
+        $class->{'set'.ucfirst($stateField)}($initialState);
+        if ($currentState != $initialState) {
+            $this->workflows->get($class)->apply($class, $currentState);
+        }
+    }
+    
+    /**
+     * Apply new workflow state
      */
     public function preUpdate(PreUpdateEventArgs $args): void
     {
@@ -48,9 +72,6 @@ class WorkflowStateListener implements EventSubscriber
         }
 
         $stateField = $this->getWorkflowName($class);
-
-        var_dump($stateField);
-
         if ($args->hasChangedField($stateField)) {
             $state = $class->{'get'.ucfirst($stateField)}();
             $class->{'set'.ucfirst($stateField)}($args->getOldValue($stateField));
@@ -65,5 +86,13 @@ class WorkflowStateListener implements EventSubscriber
         $workflowParameters = $this->container->getParameter('workflows');
 
         return $workflowParameters[end($workflowName)]['marking_store']['property'] ?? self::DEFAULT_FIELD_NAME;
+    }
+
+    private function getInitialState($class): string
+    {
+        $workflowName = explode('.', $this->arguments['configs'][get_class($class)]);
+        $workflowParameters = $this->container->getParameter('workflows');
+
+        return $workflowParameters[end($workflowName)]['initial_marking'];
     }
 }
